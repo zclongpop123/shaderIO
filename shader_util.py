b96d69dadd081686b39f82831c476ec7e14ae3f9
zangchanglong <zclongpop123@163.com>
2018-07-19 10:35:21 +0800

    combine same code to function...

--- shader_util.py
+++ shader_util.py
@@ -6,7 +6,7 @@
 import os.path
 import maya.cmds as mc
 #--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-def get_output_path(filter_format='Maya ASCII (*.ma)', start_dir=None):
+def get_start_dir(start_dir):
     '''
     '''
     if os.path.isfile(start_dir):
@@ -18,6 +18,15 @@ def get_output_path(filter_format='Maya ASCII (*.ma)', start_dir=None):
     else:
         start_dir = mc.workspace(q=True, lfw=True)[0]
 
+    return start_dir
+
+
+
+
+def get_output_path(filter_format='Maya ASCII (*.ma)', start_dir=None):
+    '''
+    '''
+    start_dir = get_start_dir(start_dir)
     filePath = mc.fileDialog2(ff=filter_format, startingDirectory=start_dir)
     return filePath
 
@@ -27,14 +36,6 @@ def get_output_path(filter_format='Maya ASCII (*.ma)', start_dir=None):
 def get_input_path(filter_format='Maya ASCII (*.ma)', start_dir=None):
     '''
     '''
-    if os.path.isfile(start_dir):
-        start_dir = os.path.dirname(start_dir)
-
-    elif os.path.isdir(start_dir):
-        pass
-
-    else:
-        start_dir = mc.workspace(q=True, lfw=True)[0]
-
+    start_dir = get_start_dir(start_dir)
     filePath = mc.fileDialog2(ff=filter_format, fm=4, okc='Select', startingDirectory=start_dir)
     return filePath
